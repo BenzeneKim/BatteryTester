456ee2c32425e17f416a7339d69a760bf6d13d62
JunSeob Kim <46683933+BenzeneKim@users.noreply.github.com>
2020-08-06 15:09:24 +0900

    Create Receiver.ino

---
+++ Receiver.ino
@@ -0,0 +1,13 @@
+#include <SoftwareSerial>
+
+SoftwareSerial BT(2,3);
+
+void setup(){
+  BT.begin(9600);
+  Serial.begin(9600);
+}
+
+void main(){
+  while(Serial.available()) Serial.print(BT.read());
+  println("\n");
+}
